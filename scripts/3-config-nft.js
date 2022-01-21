90135f70dc2d3b1ad96aa0dc995772d9b869543b
Jacob Herper <jacob@herper.uk>
2022-01-21 20:10:21 +0000

    set claim condition

---
+++ scripts/3-config-nft.js
@@ -0,0 +1,21 @@
+import sdk from './1-initialize-sdk.js';
+import { readFileSync } from 'fs';
+
+const bundleDrop = sdk.getBundleDropModule(
+	'0x405EA27acE7e537a42Bcac5a38b4A27ED388B45D'
+);
+
+(async () => {
+	try {
+		await bundleDrop.createBatch([
+			{
+				name: 'Coder',
+				description: 'This NFT will give you access to StackoverDAO',
+				image: readFileSync('scripts/assets/coder.png'),
+			},
+		]);
+		console.log('✅ Successfully created a new NFT in the drop!');
+	} catch (error) {
+		console.error('failed to create the new NFT', error);
+	}
+})();
