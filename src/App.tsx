bea0bd8f6ec742869b3bb453656d6a76af8840fe
Jacob Herper <jacob@herper.uk>
2022-01-21 19:51:10 +0000

    connected wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+
+import { useWeb3 } from '@3rdweb/hooks';
 
 const App = () => {
+	const { connectWallet, address, error, provider } = useWeb3();
+
+	const handleConnect = () => connectWallet('injected');
+
+	if (!address) {
+		return (
+			<div className="landing">
+				<h1>Welcome to StackoverDAO</h1>
+				<button onClick={handleConnect} className="btn-hero">
+					Connect your wallet
+				</button>
+			</div>
+		);
+	}
+
 	return (
 		<div className="landing">
-			<h1>Welcome to My DAO</h1>
+			<h1>Welcome to StackoverDAO</h1>
+			<p>Connected as {address}</p>
 		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
 
 import './index.css';
 
+const supportedBlockchainIds = [4]; // List of possible ids: https://besu.hyperledger.org/en/stable/Concepts/NetworkID-And-ChainID/
+
+const connectors = {
+	// Metamask is an "injected wallet"
+	injected: {},
+};
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThirdwebWeb3Provider
+			connectors={connectors}
+			supportedChainIds={supportedBlockchainIds}
+		>
+			<App />
+		</ThirdwebWeb3Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
