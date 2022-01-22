29887981803b8a18bdf72e621b5e8ea2d8b53fed
Jacob Herper <jacob@herper.uk>
2022-01-22 16:05:00 +0000

    set up vote

---
+++ scripts/9-setup-vote.js
@@ -0,0 +1,49 @@
+import { ethers } from 'ethers';
+import sdk from './1-initialize-sdk.js';
+
+// This is our governance contract.
+const voteModule = sdk.getVoteModule(
+	'0x1BcA5B5c33456C8DCc0ADa2f6E3249346122a903'
+);
+
+// This is our ERC-20 contract.
+const tokenModule = sdk.getTokenModule(
+	'0x7eFd41324f4a3666E659c42190DA0C0F6820BC91'
+);
+
+const setupVote = async () => {
+	try {
+		// Give our treasury the power to mint additional token if needed.
+		await tokenModule.grantRole('minter', voteModule.address);
+
+		console.log(
+			'Successfully gave vote module permissions to act on token module'
+		);
+	} catch (error) {
+		console.error(
+			'failed to grant vote module permissions on token module',
+			error
+		);
+		process.exit(1);
+	}
+
+	try {
+		// Grab our wallet's token balance, remember -- we hold basically the entire supply right now!
+		const ownedTokenBalance = await tokenModule.balanceOf(
+			process.env.WALLET_ADDRESS
+		);
+
+		// Grab 90% of the supply that we hold.
+		const ownedAmount = ethers.BigNumber.from(ownedTokenBalance.value);
+		const percent90 = ownedAmount.div(100).mul(90);
+
+		// Transfer 90% of the supply to our voting contract.
+		await tokenModule.transfer(voteModule.address, percent90);
+
+		console.log('✅ Successfully transferred tokens to vote module');
+	} catch (err) {
+		console.error('failed to transfer tokens to vote module', err);
+	}
+};
+
+setupVote();
